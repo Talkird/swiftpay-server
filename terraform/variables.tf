d2342c09c82674a4bc9a95c168fd79dcff69ae91
Talkird <losaurojuan@gmail.com>
2026-05-28 00:46:42 -0300

    instance change to t3.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t4g.micro"
+  default     = "t3.micro"
 }
 
 variable "instance_name" {
